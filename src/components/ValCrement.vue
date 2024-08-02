4846e204e69964c7b7066868abbfcc738a4f9c98
joseph-joshua-parker <joelnashjobs@yahoo.com>
2024-08-02 19:15:43 -0300

    crement component

---
+++ src/components/ValCrement.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <q-btn label="-" color="primary" size="xs" @click="() => crement(-1)" />
+    <q-chip>{{ val }}</q-chip>
+    <q-btn label="+" color="primary" size="xs" @click="() => crement(1)" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  emits: ['crement'],
+  props: {
+    val: { type: Number, required: true },
+    delta: { type: Number, required: true },
+  },
+
+  setup(props, ctx) {
+    const crement = (sign: number) => {
+      ctx.emit('crement', sign * props.delta);
+    };
+
+    return { crement };
+  },
+});
+</script>
