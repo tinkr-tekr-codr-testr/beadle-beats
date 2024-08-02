520d8a8ec9f87d38f59437178923a8709c74f423
joseph-joshua-parker <joelnashjobs@yahoo.com>
2024-08-02 19:17:11 -0300

    routes for config page

---
+++ src/router/routes.ts
@@ -0,0 +1,25 @@
+import { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      {
+        path: 'config',
+        component: () => import('pages/ConfigurationPage.vue'),
+      },
+
+      { path: 'index', component: () => import('pages/IndexPage.vue') },
+    ],
+  },
+
+  // Always leav this as last one,
+  // but you can also remove it
+  {
+    path: '/:catchAll(.*)*',
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
+];
+
+export default routes;
