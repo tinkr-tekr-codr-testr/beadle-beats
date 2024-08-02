c9a90b032adeafb2c349830b3bbcf22a3d52d61d
joseph-joshua-parker <joelnashjobs@yahoo.com>
2024-08-02 19:17:41 -0300

    project cleanup and setup

---
+++ src/pages/ConfigurationPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page padding>
+    <val-crement
+      @crement="(delta:number)=>shiftTempo(delta)"
+      :val="tempo"
+      :delta="5"
+    />
+  </q-page>
+</template>
+
+<script lang="ts">
+import { storeToRefs } from 'pinia';
+import ValCrement from 'src/components/ValCrement.vue';
+import { useConfigurationStore } from 'src/stores/ConfigurationStore';
+import { defineComponent } from 'vue';
+export default defineComponent({
+  components: { ValCrement },
+  setup() {
+    const configuration = useConfigurationStore();
+    const { beads, tempo } = storeToRefs(configuration);
+    const { crementBeads, shiftTempo } = configuration;
+
+    return { beads, tempo, crementBeads, shiftTempo };
+  },
+});
+</script>
