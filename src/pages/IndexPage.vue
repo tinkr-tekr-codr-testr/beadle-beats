f14046760189182c43d34382ca917d7c603b1fa8
joseph-joshua-parker <joelnashjobs@yahoo.com>
2024-08-06 14:45:24 -0300

    initial router linking

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,13 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <div>Index page</div>
+    <RouterLink to="/config">Configuration</RouterLink>
+    <RouterLink to="/session">Session</RouterLink>
   </q-page>
 </template>
 
 <script setup lang="ts">
+import { RouterLink } from 'vue-router';
+
 defineOptions({
   name: 'IndexPage',
 });
