443deca89b5b15b46d18878e984178259b1823d9
joseph-joshua-parker <joelnashjobs@yahoo.com>
2024-08-06 14:45:50 -0300

    testing volume buttons

--- src/pages/SessionPage.vue
+++ src/pages/SessionPage.vue
@@ -6,12 +6,16 @@
 import { storeToRefs } from 'pinia';
 import { useSessionBeadsStore } from 'src/stores/SessionBeadsStore';
 import { defineComponent } from 'vue';
+import { VolumeButtons } from '../../src-capacitor/node_modules/@capacitor-community/volume-buttons';
+
 export default defineComponent({
   setup() {
     const sessionBeads = useSessionBeadsStore();
     sessionBeads.makeSessionCords(13);
     const { matchesCounts, playableCords } = storeToRefs(sessionBeads);
-
+    VolumeButtons.watchVolume({}, (direction: unknown) => {
+      console.log(direction);
+    });
     console.log(playableCords.value);
 
     return { matchesCounts };
