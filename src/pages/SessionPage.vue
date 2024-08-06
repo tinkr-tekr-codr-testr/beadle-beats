1059471d73c71209dccd263af127a92214c5f914
joseph-joshua-parker <joelnashjobs@yahoo.com>
2024-08-06 14:49:57 -0300

    display vol button press on screen for testing

--- src/pages/SessionPage.vue
+++ src/pages/SessionPage.vue
@@ -1,11 +1,12 @@
 <template>
   <q-page padding> </q-page>
+  <div>{{ direction }}</div>
 </template>
 
 <script lang="ts">
 import { storeToRefs } from 'pinia';
 import { useSessionBeadsStore } from 'src/stores/SessionBeadsStore';
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import { VolumeButtons } from '../../src-capacitor/node_modules/@capacitor-community/volume-buttons';
 
 export default defineComponent({
@@ -13,8 +14,9 @@ export default defineComponent({
     const sessionBeads = useSessionBeadsStore();
     sessionBeads.makeSessionCords(13);
     const { matchesCounts, playableCords } = storeToRefs(sessionBeads);
+    const dirRef = ref('');
     VolumeButtons.watchVolume({}, (direction: unknown) => {
-      console.log(direction);
+      dirRef.value = direction as string;
     });
     console.log(playableCords.value);
 
